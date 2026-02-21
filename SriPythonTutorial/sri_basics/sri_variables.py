11b092b5dd9145459cbc41ecc4bbcfe9c4876922
HP <HP@DESKTOP-5M9UA2J>
2026-02-21 15:19:00 +0530

    Commiting

--- SriPythonTutorial/sri_basics/sri_variables.py
+++ SriPythonTutorial/sri_basics/sri_variables.py
@@ -13,6 +13,10 @@ c,d=4,5# Defining multiple variable in a single line
 print (c)
 print(d)
 
-e=f=20
+e=f=20 # Assigning single values for multiple variables
 print(e)
 print(f)
+
+Invalid cases 
+
+1. Value = variable _name 
